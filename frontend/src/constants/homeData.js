b29e4480e5c300e5053f5425cf42cba732e7d816
RUHAN-HOSSAIN <ruhanhossain207@gmail.com>
2026-03-10 02:43:01 +0600

    fix: hero image problem

--- frontend/src/constants/homeData.js
+++ frontend/src/constants/homeData.js
@@ -1,23 +1,23 @@
 
 export const heroSlides = [
   {
-    img: "./src/assets/HeroPic-0.webp",
+    img: "/demoPhoto/HeroPic-0.webp",
     title: "Shaping the Future of Technology",
     subtitle: "World-class education in Computer Science & Engineering at RUET",
   },
   {
-    img: "./src/assets/HeroPic-1.webp",
+    img: "/demoPhoto/HeroPic-1.webp",
     title: "Research That Changes the World",
     subtitle:
       "Explore cutting-edge labs, projects, and publications from our faculty and students",
   },
   {
-    img: "./src/assets/HeroPic-2.webp",
+    img: "/demoPhoto/HeroPic-2.webp",
     title: "Join a Community of Innovators",
-    // subtitle: 'From admission to graduation — we prepare you for what comes next',
+    subtitle: 'From admission to graduation — we prepare you for what comes next',
   },
   {
-    img: "./src/assets/HeroPic-3.jpg",
+    img: "/demoPhoto/HeroPic-3.jpg",
     title: "Research That Changes the World",
     subtitle: "Explore cutting-edge labs, projects, and publications from our faculty and students",
   },
