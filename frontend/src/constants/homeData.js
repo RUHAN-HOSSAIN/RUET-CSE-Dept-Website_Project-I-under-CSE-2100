115022736c901270b3f162bec9b69e9c2c8e0656
RUHAN-HOSSAIN <ruhanhossain207@gmail.com>
2026-03-10 01:57:29 +0600

    This is my official project with RUET. It's a fullstack webiste for RUET CSE Dept with MERN.

---
+++ frontend/src/constants/homeData.js
@@ -0,0 +1,24 @@
+
+export const heroSlides = [
+  {
+    img: "./src/assets/HeroPic-0.webp",
+    title: "Shaping the Future of Technology",
+    subtitle: "World-class education in Computer Science & Engineering at RUET",
+  },
+  {
+    img: "./src/assets/HeroPic-1.webp",
+    title: "Research That Changes the World",
+    subtitle:
+      "Explore cutting-edge labs, projects, and publications from our faculty and students",
+  },
+  {
+    img: "./src/assets/HeroPic-2.webp",
+    title: "Join a Community of Innovators",
+    // subtitle: 'From admission to graduation — we prepare you for what comes next',
+  },
+  {
+    img: "./src/assets/HeroPic-3.jpg",
+    title: "Research That Changes the World",
+    subtitle: "Explore cutting-edge labs, projects, and publications from our faculty and students",
+  },
+];
