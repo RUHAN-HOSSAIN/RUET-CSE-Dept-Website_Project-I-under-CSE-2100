d0f124319d1dfe38ed708f8992f95a62cacc4675
RUHAN-HOSSAIN <ruhanhossain207@gmail.com>
2026-04-15 15:12:03 +0600

    About Section baad porcelo!!

--- frontend/src/constants/homeData.js
+++ frontend/src/constants/homeData.js
@@ -34,3 +34,18 @@ export const heroSlides = [
       "Explore cutting-edge labs, projects, and publications from our faculty and students",
   },
 ];
+
+
+// Sec-2: About Section
+
+export const aboutData = {
+  description: "Computer Science & Engineering has become the resilient part of engineering discipline that combines the knowledge of computer science, computer engineering and electrical engineering to develop and integrate computer software and hardware. Computer engineers tackled many aspects of computing in software and hardware, from writing software/firmware for embedded microcontroller or developing operating systems to the design of individual microcontroller/microprocessor, personal computer, supercomputer or very basic like circuit/VLSI-Chip design.",
+
+  mission1: "To deliver contemporary educational programs through proper application and continuous dissemination of Computer Science and Engineering knowledge and train students to confront and resolve complex real-world challenges ensuring significant contribution to the prosperity of the nation and society.",
+  mission2: "To conduct cutting-edge research for the advancement in Computer Science and Engineering and allied fields by collaborating with stakeholders for creating professionals in academia, government and industry.",
+  mission3: "To create leadership by promoting ethical and virtuous values and prepare students as lifelong learners of enhanced skills and abilities to guide them for the industrial revolution.",
+
+  vision: "To become a nationwide and global center of excellence by empowering innovation, leading technological evolution, and ensuring the advancement, application, and dissemination of knowledge in diverse domains of Computer Science and Engineering via extensive educational programs, industry collaborations, research, and scholarly publications.",
+
+  headMsg: "Welcome to the Department of Computer Science & Engineering (CSE) at Rajshahi University of Engineering & Technology. Since its establishment in 1998, the department has built a strong tradition of academic excellence through the dedication of its faculty, staff, and students. The department offers a 4-year B.Sc. Engineering program in CSE, along with M.Sc. and PhD programs for local and international students. Each year, around 120 students graduate with undergraduate degrees. Our faculty members are actively involved in teaching and research, often engaging both undergraduate and graduate students in research projects."
+};
